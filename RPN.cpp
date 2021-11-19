dcb873726ebc6586d87a40b5891ec5003654323c
Denis <dennis220802@gmail.com>
2021-11-19 19:39:58 +1000

    Сделано преобразование

--- RPN.cpp
+++ RPN.cpp
@@ -1,1 +1,47 @@
 #include "RPN.h"
+#include <stack>
+#include <string>
+
+std::string RPN::infixToPostfix(const std::string &infix) {
+    std::string ans;
+    std::stack<char> operations;
+    char prev = ' ';
+
+    for (char cur: infix) {
+        if (cur == ' ') {
+            continue;
+        } else if (cur == '(') {
+            operations.push(cur);
+        } else if (cur == ')') {
+            while (!operations.empty() && operations.top() != '(') {
+                ans += ' ';
+                ans += operations.top();
+                operations.pop();
+            }
+            if (operations.empty())
+                throw std::runtime_error("Error in input");
+            operations.pop();
+        } else if (allOperators.find(cur) != allOperators.end()) {
+            if (!operations.empty() && operations.top() != '(' && allOperators[cur] <= allOperators[operations.top()]) {
+                ans += ' ';
+                ans += operations.top();
+                operations.pop();
+            }
+            operations.push(cur);
+        } else {
+            if (!(prev - '0' >= 0 && prev - '0' <= 9))
+                ans += ' ';
+            ans += cur;
+        }
+        prev = cur;
+    }
+    while (!operations.empty()) {
+        if (operations.top() == '(') {
+            throw std::runtime_error("Error in input");
+        }
+        ans += ' ';
+        ans += operations.top();
+        operations.pop();
+    }
+    return !ans.empty() ? ans.substr(1) : std::string();
+}
--- RPN.h
+++ RPN.h
@@ -2,8 +2,24 @@
 #define REVERSE_POLISH_NOTATION_RPN_H
 
 
-class RPN {
+#include <string>
+#include <map>
 
+class RPN {
+ public:
+    RPN() = default;
+
+    std::string infixToPostfix(const std::string &);
+
+ protected:
+    std::map<char, uint8_t> allOperators = {
+            {'(', 255},
+            {'+', 1},
+            {'-', 1},
+            {'*', 2},
+            {'/', 2},
+            {'^', 3},
+    };
 };
 
 
